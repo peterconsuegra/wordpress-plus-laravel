ffd69f23bbba6c852d5446958dd3b8c65876b228
Mojave <mojave@Mojaves-MacBook-Pro.local>
2020-10-26 12:09:16 -0500

    wordpress example list

--- templates/wordpress_code_example.blade.php
+++ templates/wordpress_code_example.blade.php
@@ -1,14 +1,27 @@
 <html>
+
+	<head>
+		
+		<!-- CSS -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+		<!-- jQuery and JS bundle w/ Popper.js -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+		
+	</head>
+
     <body>
-        <h1>Wordpress code example:</h1>
-		
-		<?php
-		$blogusers = get_users( array( 'fields' => array( 'display_name' ) ) );
-		// Array of stdClass objects.
-		foreach ( $blogusers as $user ) {
-		    echo '<span>' . esc_html( $user->display_name ) . '</span>';
-		}
-		?>
-		
+
+		<div class="container">
+			<br />
+		  <h3>WordPress+Laravel code examples:</h3>
+		  <ul>
+		  <li><a href="/list_users">List WordPress users</a></li>
+		  <li><a href="/list_posts">List WordPress posts</a></li>
+		  <li><a href="/list_products">List Woocommerce products</a></li>
+		  <li><a href="/list_orders">List Woocommerce orders</a></li>
+		  <li><a href="/edit_posts">Edit WordPress posts</a></li>
+	 	 </ul>
     </body>
 </html>
